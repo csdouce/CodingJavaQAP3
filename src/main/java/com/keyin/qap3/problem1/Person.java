9c723af1994a4fda5fdfe5d761169e4725a8db93
csdouce <chris.doucette@keyin.com>
2022-11-09 20:07:48 -0400

    Initial commit

---
+++ src/main/java/com/keyin/qap3/problem1/Person.java
@@ -0,0 +1,10 @@
+// Advanced Programming - Java QAP3
+// Developer: Chris Doucette
+// Date Created: Wednesday November 9, 2022
+
+package com.keyin.qap3.problem1;
+
+public class Person {
+    protected String myName;        //Name of the person
+
+}
